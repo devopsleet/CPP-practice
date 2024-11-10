fbed5781ed603be7f5c477aaab3ad1b95df8e2b1
gagan <gasingla07@gmail.com>
2024-11-10 08:41:06 -0600

    Basic CPP

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+const double pi = 3.1222;
+int myGlobal;
+
+int main() {
+    string firstName = "Gagan";
+    string lastName = "Singla";
+    std::cout << "Hello, World!" << std::endl;
+    cout<<"Test"<<endl;
+    cout<<setprecision(2)<<pi<<endl;
+    cout<<fixed;
+    cout<<setprecision(10)<<pi<<endl;
+    cout<<firstName.length()<<endl;
+
+    const int a = 10;
+
+    a = a + 1
+    return 0;
+}
