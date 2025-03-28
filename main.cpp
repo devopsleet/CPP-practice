e3a5783bf452793054d4202e60171bb3281e1e96
gagan <gasingla07@gmail.com>
2025-03-27 23:12:56 -0500

    function templates

--- main.cpp
+++ main.cpp
@@ -4,20 +4,52 @@
 
 using namespace std;
 
-int count_vowels(string s) {
-    int ans = 0;
-    for (size_t i = 0; i < s.length(); i++) {
-        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
-            ans++;
-        }
-    }
-    return ans;
+// long factorial(int n) {
+//     if (n == 0) {
+//         return 1;
+//     }
+//     else return n * factorial(n-1);
+// }
+
+// int count_vowels(string s) {
+//     int ans = 0;
+//     for (size_t i = 0; i < s.length(); i++) {
+//         if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+//             ans++;
+//         }
+//     }
+//     return ans;
+// }
+
+template <class T>
+
+T Large(T n1, T n2) {
+    return n1 > n2 ? n1 : n2;
 }
 
+
 int main() {
-    string str;
-    cin >> str;
-    cout << "The total number of vowels are " << count_vowels(str) << endl;
+
+    char c1, c2;
+
+    cin>> c1;
+    cin>>c2;
+
+    cout<<Large(c1,c2);
+
+    // int n ;
+    // cin >> n;
+    // long result = factorial(n);
+    //
+    // cout << n << "!="<< result;
+
+    // string str;
+    //
+    // int c = 0;
+    // c--;
+    // cout<<c;
+    // cin >> str;
+    // cout << "The total number of vowels are " << count_vowels(str) << endl;
 
 
     return 0;
