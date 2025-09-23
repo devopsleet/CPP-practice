aeb630a96547ea46e137263f6586e428b35682b5
gagan <gasingla07@gmail.com>
2025-09-23 11:29:32 -0500

    strings in CPP

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 using namespace std;
 
 int myGlobalNum;
 
 int main() {
 
-    int myNum;
-    string myText = "Hello";
-    cout<<myText<<endl;
-    cout<<myNum;
+    string firstname = "Alice";
+    string lastname = "Cooper";
+    string name = firstname + " " + lastname;
+    cout<<name<<endl;
+    cout<<name.find("coop")<<endl;
+    cout<<name.length()<<endl;
+    cout<<name.substr(2,3)<<endl;
     return 0;
 }
 
