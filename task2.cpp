ecad7ef5617b4dc95b5a7a4b5a7c223edab2dd08
gagan <gasingla07@gmail.com>
2025-10-02 09:11:21 -0500

    arithmetic operator

--- task2.cpp
+++ task2.cpp
@@ -8,10 +8,15 @@ int main() {
     int num1 {10};
     int num2 {20};
 
-    num1 = 100;
-    num2 = 200;
+    int res = num1 + num2;
 
-    cout<<"num1"<<num1<<endl;
+    cout<<res<<endl;
+
+
+
+
+
+
 
 
 
