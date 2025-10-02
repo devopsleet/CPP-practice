4d7fffdc18ccecf37b76a0959a772555aefb6c45
gagan <gasingla07@gmail.com>
2025-10-02 10:53:37 -0500

    initializer list inside the range based for loop

--- task2.cpp
+++ task2.cpp
@@ -5,14 +5,25 @@
 using namespace std;
 
 int main() {
-    vector<double> temp{45.6, 4354.7, 3434.8, 54656.9};
+    // vector<double> temp{45.6, 4354.7, 3434.8, 54656.9};
+    //
+    // double avg_temp{}, total{};
+    //
+    // for (auto t: temp)
+    //     total += t;
+    // cout<<fixed<<setprecision(2);
+    // cout << ((temp.size()) ? to_string(total / temp.size()) : "temp size is zero") << endl;
 
-    double avg_temp{}, total{};
+    // Initializer list in the range based for loop
 
-    for (auto t: temp)
-        total += t;
-    cout<<fixed<<setprecision(2);
-    cout << ((temp.size()) ? to_string(total / temp.size()) : "temp size is zero") << endl;
+    int total{}, size{};
+
+    for (auto temp: {1.2, 1.3, 1.7}) {
+        total += temp;
+        ++size;
+    }
+    cout<<total;
+    cout << total / size << endl;
 
     return 0;
 }
