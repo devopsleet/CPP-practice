aa3dc225f11747e1a043ba9b4cf36f1224765bc5
gagan <gasingla07@gmail.com>
2025-10-06 10:25:47 -0500

    c style strings

--- task2.cpp
+++ task2.cpp
@@ -5,25 +5,10 @@
 using namespace std;
 
 int main() {
-    // vector<double> temp{45.6, 4354.7, 3434.8, 54656.9};
-    //
-    // double avg_temp{}, total{};
-    //
-    // for (auto t: temp)
-    //     total += t;
-    // cout<<fixed<<setprecision(2);
-    // cout << ((temp.size()) ? to_string(total / temp.size()) : "temp size is zero") << endl;
 
-    // Initializer list in the range based for loop
+    char my_name [10] {'a', 1};
 
-    int total{}, size{};
-
-    for (auto temp: {1.2, 1.3, 1.7}) {
-        total += temp;
-        ++size;
-    }
-    cout<<total;
-    cout << total / size << endl;
+    cout<<my_name[0];
 
     return 0;
 }
