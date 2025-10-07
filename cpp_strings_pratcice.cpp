0ea007e8dcbe519e21a12bd5d2b049a0839424df
gagan <gasingla07@gmail.com>
2025-10-07 11:11:10 -0500

    cpp strings with bounds check

--- cpp_strings_pratcice.cpp
+++ cpp_strings_pratcice.cpp
@@ -3,10 +3,19 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cout<<"test";
+    const string s1 {"frank"};
+
+    for (const int c: s1) {
+        cout<<c<<endl;
+    }
+
+    cout<<s1.length()<<endl;
+    cout<<s1.at(5);
+
     return 0;
 }
