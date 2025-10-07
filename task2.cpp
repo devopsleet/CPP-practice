8a8cdd97096009c738a80b8360580aedeefa8536
gagan <gasingla07@gmail.com>
2025-10-07 10:25:23 -0500

    c style string comparison

--- task2.cpp
+++ task2.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
-#include <climits>
-#include <iomanip>
-#include <vector>
 #include <cstring>
+#include <cctype>
 
 using namespace std;
 
 int main() {
 
-    //char lastname[5];
-    char firstname[10] {}, lastname[5];
+    char fullname[50];
+    char temp[50];
 
-    cout<<"Enter your first name";
-    cin>>firstname;
 
-    cout<<"Enter your last name";
-    cin>>lastname;
+    cout<<"Enter your fullname = ";
+    cin.getline(fullname, 50);
 
-    cout<<static_cast<int>(firstname[0])<<endl;;
-    cout<<firstname[1]<<endl;
-    //
-     char fullname[20] {};
-    //
-    strcpy(fullname, firstname);
-    cout<<firstname<<endl;
-    cout<<fullname<<endl;
-    // strcat(fullname, " ");
-    // strcpy(fullname, lastname);
-    //
-    // cout<<fullname;
+    cout<<"Your full name is "<<fullname<<endl;
+
+    cout<<"*******************************"<<endl;
+
+    strcpy(temp, fullname);
+
+    for (size_t i {0}; i < strlen(fullname); i++) {
+
+        if (isalpha(fullname[i])) {
+            fullname[i] = static_cast<char>(toupper(fullname[i]));
+        }
+    }
+
+    cout<<"After upper case your full name is now "<<fullname<<endl;
+
+    strcpy(temp, fullname);
+    cout<<"The value oF temp is "<<temp<<endl;
+    cout<<"The value of fullname is "<<fullname<<endl;
+
+    cout<<((strcmp(temp, fullname) == 0) ?
+            "Names Matched" :
+            "Names does not match");
+
+
+
 
     return 0;
 }
