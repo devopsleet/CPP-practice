aebb5e54a93f4d17cb18adabb814132d72112991
gagan <gasingla07@gmail.com>
2025-10-27 11:19:40 -0500

    passing arrays to functions

---
+++ ArraysAndFunctions.cpp
@@ -0,0 +1,34 @@
+//
+// Created by gasin on 10/27/2025.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void print_array(int [], size_t);
+
+void set_array(int [], size_t, int);
+
+
+void print_array(int arr[], size_t size) {
+    for (size_t i{0}; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void set_array(int arr[], size_t size, int val) {
+    for (size_t i{0}; i < size; i++)
+        arr[i] = val;
+}
+
+int main() {
+    int arr[]{100, 98, 96, 94, 92};
+
+    print_array(arr, 5);
+    set_array(arr, 5, 10);
+    print_array(arr,5);
+
+    return 0;
+}
