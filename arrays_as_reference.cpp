a1a751df33e81d790cc882692b906fe1a649fbd8
gagan <gasingla07@gmail.com>
2025-10-28 21:35:15 -0500

    use of const to prevent pass by reference array modification

--- arrays_as_reference.cpp
+++ arrays_as_reference.cpp
@@ -9,13 +9,18 @@
 using namespace std;
 
 void pass_by_ref(const vector<string> &v);
+void pass_ref2(string &s);
 
 void pass_by_ref(const vector<string> &v) {
     for (auto name:v)
         cout<<name<<" ";
     cout<<endl;
-    v.at(1) = "Davis";
+    //v.at(1) = "Davis";
     //v.at(3) = "James";
+}
+
+void pass_ref2(string &s) {
+    s = "changed";
 }
 
 
@@ -25,6 +30,12 @@ int main() {
 
     pass_by_ref(three_stooges);
     pass_by_ref(three_stooges);
+
+    string t {"Frank"};
+    cout<<"The string before calling the function "<<t<<endl;
+    pass_ref2(t);
+    cout<<"The string after calling the function "<<t<<endl;
+
 
     return 0;
 }
