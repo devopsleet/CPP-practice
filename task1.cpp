e60231f87dfcf9cb23ddfda56b80a6db2a97be95
gagan <gasingla07@gmail.com>
2025-11-06 12:04:51 -0600

    max and min values

--- task1.cpp
+++ task1.cpp
@@ -6,7 +6,18 @@ using namespace std;
 
 int main() {
     cout<<"Signed int min "<<INT_MIN<<endl;
-    cout<<"Signed int max "<<INT_MAX<<endl;
+    cout<<"Signed int max "<<UINT_MAX<<endl;
+
+    cout<<sizeof( unsigned long)<<endl;
+
+    cout<<"Signed int min "<<LONG_MIN<<endl;
+    cout<<"Signed int max "<<LONG_MAX<<endl;
+
+    auto i = 020000000000;
+    cout<<typeid(i).name()<<endl;
+
+    int ans = UINT_MAX  + 1;
+    cout<<ans;
 
 
     return 0;
