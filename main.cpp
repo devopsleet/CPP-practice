1b820450ef532be3e9b0c24f5c11529f72a16ef2
gagan <gasingla07@gmail.com>
2025-11-11 20:52:58 -0600

    passing pointers to functions using const vectors

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <vector>
+
 using namespace std;
 
 // void double_data(int *int_ptr) {
@@ -8,22 +10,48 @@ using namespace std;
 // }
 
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+// void swap(int *a, int *b) {
+//     int temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+
+void display(const vector<string> *v) {
+    (*v).at(0) = "Funny";
+    for (const auto &stooge: *v) {
+        cout << stooge << " ";
+    }
+
+   // (*v).at(0) = "Funny";
 }
 
 int main() {
-    int x{100}, y{200};
+    vector<string> stooges{"Larry", "Moe", "Curly"};
+    display(&stooges);
 
-    cout << "Values before swapping ";
-    cout << x << " " << y << endl;
+    // const double *temp_ptr;
+    //
+    // double high {100};
+    // cout<<fixed<<setprecision(5)<<high<<endl;
+    // double low {67};
+    //
+    // temp_ptr = &high;
+    //
+    // cout<<*temp_ptr<<endl;
+    //
+    // temp_ptr = &low;
+    //
+    // cout<<*temp_ptr<<endl;
 
-    swap(&x, &y);
-
-    cout << "Values after swap ";
-    cout << x << " " << y << endl;
+    // int x{100}, y{200};
+    //
+    // cout << "Values before swapping ";
+    // cout << x << " " << y << endl;
+    //
+    // swap(&x, &y);
+    //
+    // cout << "Values after swap ";
+    // cout << x << " " << y << endl;
     // int value{10};
     //
     // int *int_ptr{nullptr};
