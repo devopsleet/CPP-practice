50781a075d377b8250a6316fb33d1f3a65b82b0c
gagan <gasingla07@gmail.com>
2025-11-13 14:18:01 -0600

    scope resolution operator

--- main.cpp
+++ main.cpp
@@ -22,62 +22,18 @@ void display(const vector<string> *v) {
         cout << stooge << " ";
     }
 
-   // (*v).at(0) = "Funny";
+    // (*v).at(0) = "Funny";
 }
 
+
+int reused = 42;
+
 int main() {
+    auto reused = 10;
 
-    unsigned int x = -1;
-    cout<<x<<endl;
-    //
-    // unsigned n = 11;
-    //
-    // while ( n > 0) {
-    //     --n;
-    //     cout<<n<<endl;
-    // }
+    cout << reused << endl;
 
-    // unsigned int x = -2;  // Maximum value for a 32-bit signed int
-    // std::cout << "x before overflow: " << x << std::endl;
-    //
-    // x = x + 1;        // Overflow occurs here!
-    // std::cout << "x after overflow: " << x << std::endl;
-    //
-    // x = x - 2;        // Further arithmetic
-    // std::cout << "x after subtracting 2: " << x << std::endl;
-    // vector<string> stooges{"Larry", "Moe", "Curly"};
-    // display(&stooges);
-
-    // const double *temp_ptr;
-    //
-    // double high {100};
-    // cout<<fixed<<setprecision(5)<<high<<endl;
-    // double low {67};
-    //
-    // temp_ptr = &high;
-    //
-    // cout<<*temp_ptr<<endl;
-    //
-    // temp_ptr = &low;
-    //
-    // cout<<*temp_ptr<<endl;
-
-    // int x{100}, y{200};
-    //
-    // cout << "Values before swapping ";
-    // cout << x << " " << y << endl;
-    //
-    // swap(&x, &y);
-    //
-    // cout << "Values after swap ";
-    // cout << x << " " << y << endl;
-    // int value{10};
-    //
-    // int *int_ptr{nullptr};
-    //
-    // cout << "Value" << value << endl;
-    // double_data(&value);
-    // cout << "Value: " << value << endl;
+    cout << ::reused << endl;
 
 
     return 0;
