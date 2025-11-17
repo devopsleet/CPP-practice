9c1059a1c6088499642c929447faaef152a5c759
gagan <gasingla07@gmail.com>
2025-11-17 13:25:04 -0600

    use subscript for iteration

--- cpp_strings.cpp
+++ cpp_strings.cpp
@@ -4,21 +4,36 @@
 
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
 int main() {
-    string s = "Hello World!!";
 
-    decltype(s.size()) punc_cnt = 0;
+    string s = "hell world";
 
-    for (auto c: s) {
-        if (ispunct(c)) {
-            ++punc_cnt;
-        }
+    for (decltype(s.size()) index=0; index <s.size() && !isspace(s[index]); ++index) {
+        s[index] = toupper(s[index]);
     }
 
-    cout << "Total number of punctuation characters are " << punc_cnt << endl;
+    cout<<s<<endl;
+
+    // for (auto &c: s) {
+    //     c = toupper(c);
+    // }
+    //
+    // cout<<s<<endl;
+    // string s = "Hello World!!";
+    //
+    // decltype(s.size()) punc_cnt = 0;
+    //
+    // for (auto c: s) {
+    //     if (ispunct(c)) {
+    //         ++punc_cnt;
+    //     }
+    // }
+
+    //cout << "Total number of punctuation characters are " << punc_cnt << endl;
 
 
     return 0;
