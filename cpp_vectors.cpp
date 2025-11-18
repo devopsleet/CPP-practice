f9cde7559de8e03b275d9b58d2cd4e0a5e974579
gagan <gasingla07@gmail.com>
2025-11-18 09:32:35 -0600

    vectors push back method

--- cpp_vectors.cpp
+++ cpp_vectors.cpp
@@ -2,3 +2,19 @@
 // Created by gasin on 10/30/2025.
 //
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> v1;
+
+    for (size_t i{0}; i < 100; ++i) {
+        v1.push_back(i);
+    }
+
+    cout << v1.at(99);
+
+
+    return 0;
+}
