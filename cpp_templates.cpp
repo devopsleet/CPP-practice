229e936573b78ed8e25839d9c47c10b5bc648c44
gagan <gasingla07@gmail.com>
2025-12-10 10:56:33 -0600

    vector operations

--- cpp_templates.cpp
+++ cpp_templates.cpp
@@ -10,14 +10,36 @@
 using namespace std;
 
 
-template<typename T>
-T myMax(T a, T b) {
-    return (a > b) ? a : b;
-}
-
-
 int main() {
-    cout << myMax<int>(3, 4) << endl;
+    vector<char> v {'a', 'f', 'd'};
+    vector<string> v2;
+
+    v.push_back('e');
+
+    v.insert(v.begin() + 1, 'c');
+
+    // for (auto c: v) {
+    //     cout<<c<<endl;
+    // }
+    //
+    // cout<<v[4]<<endl;
+    cout<<v.size()<<endl;
+    v[7] = 'h';
+    // v2[0] = "hi";
+    // v2.push_back("gagan");
+    // cout<<v2.at(0)<<endl;
+    //v2.at(1) = "hello";
+
+    //cout<<v[7]<<endl;
+
+
+    //cout<<v.at(7)<<endl;
+
+    cout<<v.size();
+
+    v.pop_back();
+
+    for (auto c: v) cout<<c<<endl;
 
     return 0;
 }
