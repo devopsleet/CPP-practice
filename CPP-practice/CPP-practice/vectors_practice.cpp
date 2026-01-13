0161229db164836a2224d3bb54741a81d48c9568
gagan <gasingla07@gmail.com>
2026-01-13 16:54:01 -0600

    functions called by reference

--- CPP-practice/CPP-practice/vectors_practice.cpp
+++ CPP-practice/CPP-practice/vectors_practice.cpp
@@ -3,22 +3,32 @@
 #include <string>
 using namespace std;
 
+size_t find_char(const string &s, char c, size_t &occur) {
+    
+    auto ret = -1;
+    occur = 0;
+    for(size_t i {0}; i < s.size(); ++i) {
+        if (s[i] == c) {
+            if (ret == -1)
+                ret = i;
+            ++occur;
+        }
+           
+            
+    }
+    
+    
+    return ret;
+    
+    
+}
+
 int main() {
+    size_t ctr;
+    int idx = find_char("Gagan", 'o', ctr);
     
-    string s("some string");
-    
-    if (s.begin() != s.end()) {
-        auto it = s.begin();
-        *it = toupper(*it);
-    }
-    
-    for(auto it = s.begin(); !isspace(*it) && it!= s.end(); ++it) {
-        *it = toupper(*it);
-    }
-    
-    
-    
-    cout<<s<<endl;
+    cout<<"The index value is " <<idx<<endl;
+    cout<<"The number of occurences are "<<ctr<<endl;
 
     return 0;
 }
