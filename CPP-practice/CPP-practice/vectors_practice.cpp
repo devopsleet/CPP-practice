05f15d5de7e8ac304ebc3339141ec09cc2ca703c
gagan <gasingla07@gmail.com>
2026-01-14 10:52:14 -0600

    constant pointers

--- CPP-practice/CPP-practice/vectors_practice.cpp
+++ CPP-practice/CPP-practice/vectors_practice.cpp
@@ -2,33 +2,53 @@
 #include <vector>
 #include <string>
 using namespace std;
-
-size_t find_char(const string &s, char c, size_t &occur) {
-    
-    auto ret = -1;
-    occur = 0;
-    for(size_t i {0}; i < s.size(); ++i) {
-        if (s[i] == c) {
-            if (ret == -1)
-                ret = i;
-            ++occur;
-        }
-           
-            
-    }
-    
-    
-    return ret;
-    
-    
-}
+//
+//size_t find_char(const string &s, char c, size_t &occur) {
+//    
+//    auto ret = -1;
+//    occur = 0;
+//    for(size_t i {0}; i < s.size(); ++i) {
+//        if (s[i] == c) {
+//            if (ret == -1)
+//                ret = i;
+//            ++occur;
+//        }
+//           
+//            
+//    }
+//    
+//    
+//    return ret;
+//    
+//    
+//}
 
 int main() {
-    size_t ctr;
-    int idx = find_char("Gagan", 'o', ctr);
     
-    cout<<"The index value is " <<idx<<endl;
-    cout<<"The number of occurences are "<<ctr<<endl;
+    
+    
+    int x = 10;
+    int y = 20;
+    
+    const int *p = &x;
+    //*p = 15;
+    
+    p = &y;
+    cout<<*p<<endl;
+    
+    int* const z = &x;
+    
+    *z = 15;
+    
+    cout<<*z<<endl;
+    
+
+
+//    size_t ctr;
+//    int idx = find_char("Gagan", 'o', ctr);
+//    
+//    cout<<"The index value is " <<idx<<endl;
+//    cout<<"The number of occurences are "<<ctr<<endl;
 
     return 0;
 }
