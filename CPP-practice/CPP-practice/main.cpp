1b3e2b0735fa2d6532ed86637637e4a54d1e2f56
gagan <gasingla07@gmail.com>
2026-01-14 16:56:37 -0600

    assignment operators

--- CPP-practice/CPP-practice/main.cpp
+++ CPP-practice/CPP-practice/main.cpp
@@ -1,7 +1,28 @@
-//#include <stdio.h>
-//
-//int main(int argc, char **argv)
-//{
-//	printf("hello world\n");
-//	return 0;
-//}
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num1 {10};
+    int num2 {20};
+    
+    num1 = 100; // assignment
+    
+    num2 = 200; //assignment
+    
+    num1 = num2 = 1000;
+    
+    int result {0};
+    
+    cout<<num1<< "+" <<
+    
+    cout<<"num1 "<<num1<<endl;
+    cout<<"num2 "<<num2<<endl;
+    
+    
+    
+    
+    return 0;
+    
+}
