8ca3ead138ab84a674c79e54577feb07d9f84e16
gagan <gasingla07@gmail.com>
2026-02-02 21:11:24 -0600

    add enum with switch statement

--- CPP-practice/CPP-practice/main.cpp
+++ CPP-practice/CPP-practice/main.cpp
@@ -4,46 +4,27 @@ using namespace std;
 
 int main() {
     
-    char letter_grade{};
     
-    cout<<"Enter the letter grade you expect on the exam ";
-    cin>>letter_grade;
+    enum Direction {
+        left, right, up, down
+    };
     
-    switch(letter_grade) {
-        case 'a':
-        case 'A':
-            cout<<"You need 90 or above"<<endl;
+    Direction heading {down};
+    
+    switch(heading) {
+        case left:
+            cout<<"Going left"<<endl;
             break;
-        case 'b':
-        case 'B':
-            cout<<"You need 80-89"<<endl;
+        case 1:
+            cout<<"Right";
             break;
-        case 'c':
-        case 'C':
-            cout<<"You need 70-79"<<endl;
+        case 2:
+            cout<<"Up"<<endl;
             break;
-        case 'd':
-        case 'D':
-            cout<<"You should study hard"<<endl;
+        case 3:
+            cout<<"Down"<<endl;
             break;
-        case 'f':
-        case 'F':
-        {
-            char confirm{};
-            
-            cout<<"Are you sure?";
-            cin>>confirm;
-            
-            if (confirm == 'y' || confirm == 'Y')
-                cout<<"OK";
-            else if (confirm == 'n' || confirm == 'N')
-                cout<<"Good";
-            else
-                cout<<"Failed";
-            break;
-        }
-        default:
-            cout<<"Sorry, not a valid grade"<<endl;
+        
     }
     
     
