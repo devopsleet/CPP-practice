d13bc1a5f959e729f0ee24686f7c8b4fdddd596d
gagan <gasingla07@gmail.com>
2026-02-02 21:04:09 -0600

    switch statement

--- CPP-practice/CPP-practice/main.cpp
+++ CPP-practice/CPP-practice/main.cpp
@@ -4,13 +4,47 @@ using namespace std;
 
 int main() {
     
-    double a = 100;
+    char letter_grade{};
     
-    int b = 12;
+    cout<<"Enter the letter grade you expect on the exam ";
+    cin>>letter_grade;
     
-    int x = a/b;
-    
-    cout<<x;
+    switch(letter_grade) {
+        case 'a':
+        case 'A':
+            cout<<"You need 90 or above"<<endl;
+            break;
+        case 'b':
+        case 'B':
+            cout<<"You need 80-89"<<endl;
+            break;
+        case 'c':
+        case 'C':
+            cout<<"You need 70-79"<<endl;
+            break;
+        case 'd':
+        case 'D':
+            cout<<"You should study hard"<<endl;
+            break;
+        case 'f':
+        case 'F':
+        {
+            char confirm{};
+            
+            cout<<"Are you sure?";
+            cin>>confirm;
+            
+            if (confirm == 'y' || confirm == 'Y')
+                cout<<"OK";
+            else if (confirm == 'n' || confirm == 'N')
+                cout<<"Good";
+            else
+                cout<<"Failed";
+            break;
+        }
+        default:
+            cout<<"Sorry, not a valid grade"<<endl;
+    }
     
     
     
